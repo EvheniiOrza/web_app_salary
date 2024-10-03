02254a81c534b446bff95b3c9f3d14c6d56c0689
Yevhenii <146078178+EvheniiOrza@users.noreply.github.com>
2024-10-03 19:24:40 +0300

    mod commit

--- payroll_system/payroll_system/settings.py
+++ payroll_system/payroll_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jq_a*8&qj&e%_6mq+k&pu+(a)vkve2!(l@ed9yysd240pi^-%t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
